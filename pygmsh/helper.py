8a66622f4da6e34c1570f32fde05b32771117c8f
Brian Drawert <bdrawert@cs.ucsb.edu>
2016-05-11 20:39:17 -0700

    updating generate_mesh function

--- pygmsh/helper.py
+++ pygmsh/helper.py
@@ -25,23 +25,50 @@ def rotation_matrix(u, theta):
     return R
 
 
-def generate_mesh(geo_object, optimize=True):
+#def generate_mesh(geo_object, optimize=True):
+#    import meshio
+#    import os
+#    import subprocess
+#    import tempfile
+#
+#    handle, filename = tempfile.mkstemp(suffix='.geo')
+#    os.write(handle, geo_object.get_code())
+#    os.close(handle)
+#
+#    handle, outname = tempfile.mkstemp(suffix='.msh')
+#
+#    cmd = ['gmsh', '-3', filename, '-o', outname]
+#    if optimize:
+#        cmd += ['-optimize']
+#    out = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+#    print(out)
+#
+#    points, cells, _, _, _ = meshio.read(outname)
+#
+#    return points, cells
+
+# Originally from: pygmsh/helper.py
+def generate_mesh(geo_object, optimize=True, return_msh_fh=False, verbose=False):
     import meshio
     import os
     import subprocess
     import tempfile
 
-    handle, filename = tempfile.mkstemp(suffix='.geo')
-    os.write(handle, geo_object.get_code())
-    os.close(handle)
+    geo_handle = tempfile.NamedTemporaryFile(suffix='.geo', delete=True)
+    geo_handle.write(geo_object.get_code())
+    geo_handle.flush()
 
-    handle, outname = tempfile.mkstemp(suffix='.msh')
+    msh_handle  = tempfile.NamedTemporaryFile(suffix='.msh', delete=True)
 
-    cmd = ['gmsh', '-3', filename, '-o', outname]
+    cmd = ['gmsh', '-3', geo_handle.name, '-o', msh_handle.name]
     if optimize:
         cmd += ['-optimize']
     out = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-    print(out)
+    if verbose:
+        print(out)
+    
+    if return_msh_fh:
+        return msh_handle
 
     points, cells, _, _, _ = meshio.read(outname)
 
